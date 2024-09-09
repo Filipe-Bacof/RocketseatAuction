555e57e3c9a623cfee4ec546685a7cd7a6fc0d2c
Filipe Bacof <filipebacof@gmail.com>
2024-09-09 17:10:16 -0300

    aula 2 backend em C#

---
+++ src/RocketseatAuction.API/Controllers/OfferController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.API.Communication.Requests;
+using RocketseatAuction.API.Filters;
+using RocketseatAuction.API.UseCases.Offers.CreateOffer;
+
+namespace RocketseatAuction.API.Controllers;
+
+// Colocando esta linha antes da classe faz com que todos os endpoints sejam necessário autenticar.
+[ServiceFilter(typeof(AuthenticationUserAttribute))]
+public class OfferController : RocketseatAuctionBaseController
+{
+    [HttpPost]
+    [Route("{itemId}")]
+    public IActionResult CreateOffer(
+        [FromRoute]int itemId,
+        [FromBody]RequestCreateOfferJson request,
+        [FromServices] CreateOfferUseCase useCase
+    )
+    {
+        var id = useCase.Execute(itemId, request);
+
+        return Created(string.Empty, id);
+    }
+}
