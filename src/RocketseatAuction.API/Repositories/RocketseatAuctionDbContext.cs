440ba765412ac5eff36aef3542a5312455e2cf28
Filipe Bacof <filipebacof@gmail.com>
2024-09-08 22:23:59 -0300

    aula 1 backend em C#

---
+++ src/RocketseatAuction.API/Repositories/RocketseatAuctionDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+
+namespace RocketseatAuction.API.Repositories;
+
+public class RocketseatAuctionDbContext : DbContext
+{
+    public DbSet<Auction> Auctions { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Os dois jeitos de inserir "\" em uma string:
+        // optionsBuilder.UseSqlite("Data Source=C:\\Workspace\\RocketseatAuction\\leilaoDbNLW.db");
+        // optionsBuilder.UseSqlite(@"Data Source=C:\Workspace\RocketseatAuction\leilaoDbNLW.db");
+
+        optionsBuilder.UseSqlite(@"Data Source=C:\Workspace\RocketseatAuction\leilaoDbNLW.db");
+    }
+}
