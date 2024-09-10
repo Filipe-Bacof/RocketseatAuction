ab92b096bc71cb423beb780dfb73c578f97c82ae
Filipe Bacof <filipebacof@gmail.com>
2024-09-09 21:13:14 -0300

    refatoração de repositories

--- src/RocketseatAuction.API/Repositories/RocketseatAuctionDbContext.cs
+++ src/RocketseatAuction.API/Repositories/RocketseatAuctionDbContext.cs
@@ -5,16 +5,8 @@ namespace RocketseatAuction.API.Repositories;
 
 public class RocketseatAuctionDbContext : DbContext
 {
+    public RocketseatAuctionDbContext(DbContextOptions options) : base(options) { }
     public DbSet<Auction> Auctions { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<Offer> Offers { get; set; }
-    
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        // Os dois jeitos de inserir "\" em uma string:
-        // optionsBuilder.UseSqlite("Data Source=C:\\Workspace\\RocketseatAuction\\leilaoDbNLW.db");
-        // optionsBuilder.UseSqlite(@"Data Source=C:\Workspace\RocketseatAuction\leilaoDbNLW.db");
-
-        optionsBuilder.UseSqlite(@"Data Source=C:\Workspace\RocketseatAuction\leilaoDbNLW.db");
-    }
 }
--- src/RocketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
+++ src/RocketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -1,21 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using RocketseatAuction.API.Contracts;
 using RocketseatAuction.API.Entities;
-using RocketseatAuction.API.Repositories;
 
 namespace RocketseatAuction.API.UseCases.Auctions.GetCurrent;
 
 public class GetCurrentAuctionUseCase
 {
-    public Auction? Execute()
-    {
-        var repository = new RocketseatAuctionDbContext();
-
-        var today = DateTime.Now;
-        // var today = new DateTime(2024, 05, 01);
-
-        return repository
-            .Auctions
-            .Include(auction => auction.Items)
-            .FirstOrDefault(auction => today >= auction.Starts && today <= auction.Ends);
-    }
+    private readonly IAuctionRepository _repository;
+    public GetCurrentAuctionUseCase(IAuctionRepository repository) => _repository = repository;
+    public Auction? Execute() => _repository.GetCurrent();
 }
